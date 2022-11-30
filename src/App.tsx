b0c60f55987157bc96073c56d2190681f6b0d674
graift-off <graf.off.it@gmail.com>
2022-11-30 11:29:49 +0200

    feat: added two routes and empty components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import './style.scss';
+import { Route, Routes } from 'react-router-dom';
+import { CatalogPage } from './components/CatalogPage/CatalogPage';
+import { HomePage } from './components/HomePage/HomePage';
 
 const App: React.FC = () => (
-  <h1 className="App">
-    Hello Palyanytsi
-  </h1>
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="catalog" element={<CatalogPage />} />
+
+      <Route
+        path="*"
+        element={
+          <p>Page not found</p>
+        }
+      />
+    </Routes>
+  </div>
 );
 
 export default App;

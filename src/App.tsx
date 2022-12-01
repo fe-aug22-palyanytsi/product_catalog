1f68dbf9ccfa02d56965ffd5dac12c0fc6227953
graift-off <graf.off.it@gmail.com>
2022-12-01 13:11:25 +0200

    added some styles

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const App: React.FC = () => (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="phones" element={<PhonesPage />} />
-
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </div>

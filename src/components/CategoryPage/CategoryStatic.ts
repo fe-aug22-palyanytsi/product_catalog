0424d726474113265ef576788d0b9a06acbc8250
RomaVarkov <varkovroma@gmail.com>
2022-12-05 12:03:36 +0200

    Caterory page

---
+++ src/components/CategoryPage/CategoryStatic.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    src: '../../assets/img/Category/category_3.jpg',
+    type: 'Mobile phones',
+    count: 95,
+  },
+  {
+    src: '../../assets/img/Category/category_3.jpg',
+    type: 'Tablets',
+    count: 24,
+  },
+  {
+    src: '../../assets/img/Category/category_3.jpg',
+    type: 'Accessories',
+    count: 100,
+  },
+];
